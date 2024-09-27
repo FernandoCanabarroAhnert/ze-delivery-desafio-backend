ea61196599fda7cef355119081a6a93c03782e28
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2024-09-27 20:22:05 -0300

    :tada: init: Projeto Criado

---
+++ src/main/java/com/fernandocanabarro/desafio_ze_delivery/DesafioZeDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.desafio_ze_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioZeDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioZeDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandocanabarro/desafio_ze_delivery/DesafioZeDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.desafio_ze_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioZeDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
